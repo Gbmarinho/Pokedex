b2a85b0b9877e2ea704e5fc2f870fd15efeac8de
Gabriel Marinho <gabriel.marinho@injunior.com.br>
2023-02-05 16:15:09 -0300

    feat: terminando search

--- app2.js
+++ app2.js
@@ -1,48 +1,12 @@
-// API endpoint --------------------------------------------
-const fetchConfig = {
-    method: 'GET'
-}
+var id = localStorage.getItem('algumaDataEspecial');
 const baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
-const PriGeracao = document.querySelector('#PrimeiraGeracao');
-const SegGeracao = document.querySelector('#SegundaGeracao');
-const TerGeracao = document.querySelector('#TerceiraGeracao');
-const QuarGeracao = document.querySelector('#QuartaGeracao');
-const QuinGeracao = document.querySelector('#QuintaGeracao');
-const SexGeracao = document.querySelector('#SextaGeracao');
-const SetGeracao = document.querySelector('#SetimaGeracao');
-const OitGeracao = document.querySelector('#OitavaGeracao');
+const PriGeracao = document.querySelector('#Procurado');
 const body = document.querySelector('.ios');
-
 function remove(check){
     var div3 = check.parentNode.parentNode;
     div3.children[0].remove();
 }
 
-function busca(){
-    var input = document.getElementById("searchbar");
-    var texto = input.value;
-    localStorage.setItem('algumaDataEspecial', texto);
-}
-
-$('.busca').click(function(e){
-    busca();
-})
-document.addEventListener("keypress", (e) => {
-    if (e.key === "Enter") {
-        busca();
-        var btn = document.querySelector(".redireciona");
-        btn.click();
-    }
-});
-// document.addEventListener("keypress", function(e) {
-//     if(e.key === 'Enter') {
-    
-//     var btn = document.querySelector("#searchbar");
-      
-//       btn.click();
-    
-//     }
-//   });
 async function createViewPokemon(div,data){
     const div2 = document.createElement('div');
     const header  = document.querySelector("#header-home");
@@ -489,7 +453,6 @@ async function mostraPokemon(div, data){
         })
         .catch(error => console.error(error))
 }
-
 function getMobileOperatingSystem() {
     var userAgent = navigator.userAgent || navigator.vendor || window.opera;
   
@@ -519,6 +482,7 @@ async function limpar(alvo) {
     // Remove todos os descendentes da <div id="alvo">
     alvo.innerText = "";
 }
+
 
 async function createPokemonate9(data, geracao, num){
     const div = document.createElement('div');
@@ -603,270 +567,25 @@ async function createPokemon(data, geracao){
 }
 
 async function getPokemon1geracao(){
-    for(let i = 1; i <= 151; i++){
-        
+    for(let i = 1; i <= 897; i++){
         await fetch(`${baseUrl}` + String(i))
         .then(response => response.json())
         .then(data => {
-            if(i<10){
-                var a = "00"+ String(i)
-                createPokemonate9(data, PriGeracao, a);
-            }if(i>9 && i<100){
-                var a = "0"+ String(i)
-                createPokemonate9(data, PriGeracao, a);
-            }if(i>99){
-                createPokemon(data, PriGeracao);
+            if(data.name.includes(id)){
+                if(i<10){
+                    var a = "00"+ String(i)
+                    createPokemonate9(data, PriGeracao, a);
+                }if(i>9 && i<100){
+                    var a = "0"+ String(i)
+                    createPokemonate9(data, PriGeracao, a);
+                }if(i>99){
+                    createPokemon(data, PriGeracao);
+                }
             }
+            
             
         })
         .catch(error => console.error(error))
      }
 }
-async function getPokemon2geracao(){
-    for(let i = 152; i <= 251; i++){
-        await fetch(`${baseUrl}` + String(i))
-        .then(response => response.json())
-        .then(data => {
-            createPokemon(data, SegGeracao);
-        })
-        .catch(error => console.error(error))
-     }
-}
-async function getPokemon3geracao(){
-    for(let i = 252; i <= 386; i++){
-        await fetch(`${baseUrl}` + String(i))
-        .then(response => response.json())
-        .then(data => {
-            createPokemon(data, TerGeracao);
-        })
-        .catch(error => console.error(error))
-     }
-}
-async function getPokemon4geracao(){
-    for(let i = 387; i <= 493; i++){
-        await fetch(`${baseUrl}` + String(i))
-        .then(response => response.json())
-        .then(data => {
-            createPokemon(data, QuarGeracao);
-        })
-        .catch(error => console.error(error))
-     }
-}
-async function getPokemon5geracao(){
-    for(let i = 494; i <= 649; i++){
-        await fetch(`${baseUrl}` + String(i))
-        .then(response => response.json())
-        .then(data => {
-            createPokemon(data, QuinGeracao);
-        })
-        .catch(error => console.error(error))
-     }
-}
-async function getPokemon6geracao(){
-    for(let i = 650; i <= 721; i++){
-        await fetch(`${baseUrl}` + String(i))
-        .then(response => response.json())
-        .then(data => {
-            createPokemon(data, SexGeracao);
-        })
-        .catch(error => console.error(error))
-     }
-}
-async function getPokemon7geracao(){
-    for(let i = 722; i <= 807; i++){
-        await fetch(`${baseUrl}` + String(i))
-        .then(response => response.json())
-        .then(data => {
-            createPokemon(data, SetGeracao);
-        })
-        .catch(error => console.error(error))
-     }
-}
-async function getPokemon8geracao(){
-    for(let i = 808; i <= 897; i++){
-        await fetch(`${baseUrl}` + String(i))
-        .then(response => response.json())
-        .then(data => {
-            createPokemon(data, OitGeracao);
-        })
-        .catch(error => console.error(error))
-     }
-}
-
-
-
-
-$('.oitGeracao').click(async function(e){
-    $('#PrimeiraGeracao').css("opacity", "0");
-    limpar(PriGeracao)
-    $('#SegundaGeracao').css("opacity", "0");
-    limpar(SegGeracao)
-    $('#TerceiraGeracao').css("opacity", "0");
-    limpar(TerGeracao)
-    $('#QuartaGeracao').css("opacity", "0");
-    limpar(QuarGeracao)
-    $('#QuintaGeracao').css("opacity", "0");
-    limpar(QuinGeracao)
-    $('#SextaGeracao').css("opacity", "0");
-    limpar(SexGeracao)
-    $('#SetimaGeracao').css("opacity", "0");
-    limpar(SetGeracao)
-
-    getPokemon8geracao()
-    $('#OitavaGeracao').css("opacity", "1");
-})
-$('.priGeracao').click(async function(e){
-    $('#SegundaGeracao').css("opacity", "0");
-    limpar(SegGeracao)
-    $('#TerceiraGeracao').css("opacity", "0");
-    limpar(TerGeracao)
-    $('#QuartaGeracao').css("opacity", "0");
-    limpar(QuarGeracao)
-    $('#QuintaGeracao').css("opacity", "0");
-    limpar(QuinGeracao)
-    $('#SextaGeracao').css("opacity", "0");
-    limpar(SexGeracao)
-    $('#SetimaGeracao').css("opacity", "0");
-     limpar(SetGeracao)
-    $('#OitavaGeracao').css("opacity", "0");
-    limpar(OitGeracao)
-
-    getPokemon1geracao()
-    $('#PrimeiraGeracao').css("opacity", "1");
-})
-
-$('.segGeracao').click(async function(e){
-    $('#PrimeiraGeracao').css("opacity", "0");
-    limpar(PriGeracao)
-    $('#TerceiraGeracao').css("opacity", "0");
-    limpar(TerGeracao)
-    $('#QuartaGeracao').css("opacity", "0");
-    limpar(QuarGeracao)
-    $('#QuintaGeracao').css("opacity", "0");
-    limpar(QuinGeracao)
-    $('#SextaGeracao').css("opacity", "0");
-    limpar(SexGeracao)
-    $('#SetimaGeracao').css("opacity", "0");
-    limpar(SetGeracao)
-    $('#OitavaGeracao').css("opacity", "0");
-    limpar(OitGeracao)
-
-    getPokemon2geracao()
-    $('#SegundaGeracao').css("opacity", "1");
-})
-
-$('.terGeracao').click(async function(e){
-    $('#PrimeiraGeracao').css("opacity", "0");
-    limpar(PriGeracao)
-    $('#SegundaGeracao').css("opacity", "0");
-    limpar(SegGeracao)
-    $('#QuartaGeracao').css("opacity", "0");
-    limpar(QuarGeracao)
-    $('#QuintaGeracao').css("opacity", "0");
-    limpar(QuinGeracao)
-    $('#SextaGeracao').css("opacity", "0");
-    limpar(SexGeracao)
-    $('#SetimaGeracao').css("opacity", "0");
-    limpar(SetGeracao)
-    $('#OitavaGeracao').css("opacity", "0");
-    limpar(OitGeracao)
-
-    getPokemon3geracao()
-    $('#TerceiraGeracao').css("opacity", "1");
-})
-$('.quaGeracao').click(async function(e){
-    $('#PrimeiraGeracao').css("opacity", "0");
-    limpar(PriGeracao)
-    $('#SegundaGeracao').css("opacity", "0");
-    limpar(SegGeracao)
-    $('#TerceiraGeracao').css("opacity", "0");
-    limpar(TerGeracao)
-    $('#QuintaGeracao').css("opacity", "0");
-    limpar(QuinGeracao)
-    $('#SextaGeracao').css("opacity", "0");
-    limpar(SexGeracao)
-    $('#SetimaGeracao').css("opacity", "0");
-    limpar(SetGeracao)
-    $('#OitavaGeracao').css("opacity", "0");
-    limpar(OitGeracao)
-
-    getPokemon4geracao()
-    $('#QuartaGeracao').css("opacity", "1");
-})
-
-$('.quinGeracao').click(async function(e){
-    $('#PrimeiraGeracao').css("opacity", "0");
-    limpar(PriGeracao)
-    $('#SegundaGeracao').css("opacity", "0");
-    limpar(SegGeracao)
-    $('#TerceiraGeracao').css("opacity", "0");
-    limpar(TerGeracao)
-    $('#QuartaGeracao').css("opacity", "0");
-    limpar(QuarGeracao)
-    $('#SextaGeracao').css("opacity", "0");
-    limpar(SexGeracao)
-    $('#SetimaGeracao').css("opacity", "0");
-    limpar(SetGeracao)
-    $('#OitavaGeracao').css("opacity", "0");
-    limpar(OitGeracao)
-
-    getPokemon5geracao()
-    $('#QuintaGeracao').css("opacity", "1");
-})
-
-$('.sextaGeracao').click(async function(e){
-    $('#PrimeiraGeracao').css("opacity", "0");
-    limpar(PriGeracao)
-    $('#SegundaGeracao').css("opacity", "0");
-    limpar(SegGeracao)
-    $('#TerceiraGeracao').css("opacity", "0");
-    limpar(TerGeracao)
-    $('#QuartaGeracao').css("opacity", "0");
-    limpar(QuarGeracao)
-    $('#QuintaGeracao').css("opacity", "0");
-    limpar(QuinGeracao)
-    $('#SetimaGeracao').css("opacity", "0");
-    limpar(SetGeracao)
-    $('#OitavaGeracao').css("opacity", "0");
-    limpar(OitGeracao)
-
-    getPokemon6geracao()
-    $('#SextaGeracao').css("opacity", "1");
-})
-$('.setGeracao').click(async function(e){
-    $('#PrimeiraGeracao').css("opacity", "0");
-    limpar(PriGeracao)
-    $('#SegundaGeracao').css("opacity", "0");
-    limpar(SegGeracao)
-    $('#TerceiraGeracao').css("opacity", "0");
-    limpar(TerGeracao)
-    $('#QuartaGeracao').css("opacity", "0");
-    limpar(QuarGeracao)
-    $('#QuintaGeracao').css("opacity", "0");
-    limpar(QuinGeracao)
-    $('#SextaGeracao').css("opacity", "0");
-    limpar(SexGeracao)
-    $('#OitavaGeracao').css("opacity", "0");
-    limpar(OitGeracao)
-
-    getPokemon7geracao()
-    $('#SetimaGeracao').css("opacity", "1");
-})
-
-var cont2 = 0;
 getPokemon1geracao()
-$('#btn-menu').click(function(e){
-    if(cont2 % 2 == 0){
-        
-        $('.menu-escondido').css("transform", "scaleY(1)");
-    }else{
-
-        $('.menu-escondido').css("transform", "scaleY(0)");
-    }
-    cont2++; 
-})
-
-function animar(){
-    const btn = document.getElementById("btn-menu");
-    btn.classList.toggle('ativar');
-}
